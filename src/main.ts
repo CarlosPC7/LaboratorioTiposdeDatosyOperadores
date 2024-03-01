1a63753d0453276f1631e75cc4fc26e79ad00d27
Carlos PC7 <formacionescpc@gmail.com>
2024-03-01 09:58:24 +0100

    restaurante carpetas

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+
+console.log("Hello Typescript!");
+console.log("Coste comida:");
+console.log(120-18);
+console.log("Cuánto debe pagar cada comensal en total:");
+console.log(120/6);
+console.log("Cuánto debe pagar cada comensal en comida:");
+console.log((120-18)/6)
+console.log("Cuánto debe pagar cada comensal en bebida:");
+console.log(18/6);
